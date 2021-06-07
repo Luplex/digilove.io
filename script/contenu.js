ab30919d72efc5ecd4b691241da2ea712f9b6fb1
Luplex <60183000+Luplex@users.noreply.github.com>
2021-06-07 07:25:06 +0200

    flip image data

--- script/contenu.js
+++ script/contenu.js
@@ -1,5 +1,3 @@
-
-
 // slider
 var swiper = new Swiper('.swiper-container', {
     effect: 'coverflow',
@@ -40,21 +38,14 @@ window.addEventListener("scroll", function (e){
 }, false);
 
 //open card
-/*
-$document.ready(function(){
-  if($(".swiper-slide").click){
-    $(this).addClass("swiper-slide_open");
-    $(this).children(".book_container").addClass("book_container_open");
-    $(this).children(".card_open").addClass(".card_open");
-    $(this).children(".card_open").children(".slider-text").addClass(".slider-text_open");
-  }
-})
-*/
+
+//card reverse
 $(document).ready(function(){
   
 
   $(".swiper-slide").click(function(){
     $(this).toggleClass("swiper-slide_open");
+    $(this).find(".img_content").css("display","block")
     $(this).find(".book_container").toggleClass("book_container_open");
     $(this).find(".card").toggleClass("card_open").find(".slider-text").toggleClass("slider-text_open")
     .find(".contenu_textuel").toggleClass("contenu_textuel_open");
@@ -71,56 +62,12 @@ $(document).ready(function(){
     
   });
 
- /* $(".cross").click(function(){
-    
-    $(this).parent(".swiper-slide_open").css("display","none").removeClass().addClass("swiper-slide").css("display","flex");
-    $("contenu_textuel").css("display","none");
-    /*$(this).parent().children(".book_container_open").removeClass().addClass("book_container");
-    $(this).parent().children(".card_open").removeClass().addClass("card");
-    $(this).parent().children(".card").children(".slider-text_open").removeClass().addClass("slider-text");      
-    $(this).parent().children(".card").children(".slider-categorie_open").removeClass().addClass("slider-categorie");
-    $(this).parent().children(".card").children(".slider-categorie").children(".imgInContent").css("display","none");
-    $(this).parent().children(".card").children(".slider-categorie").children(".imgInBanner").css("display","block");
-    $(this).css("display","none");*/
-    console.log("hello");
-       
-
- // });
+ 
  
 })
 
 
-$( document ).ready(function() {
-     var el = document.querySelector('.like_button'),
-    // mo.js timeline obj
-    timeline = new mojs.Timeline(),
-  
-    // tweens for the animation:
-  
-    // burst animation
-  
-  
-    tween3 = new mojs.Burst({
-    parent: el,
-    radius:   { 0: 100 },
-    angle:    { 0: -45 },
-    y: -10,
-    count:    10,
-     radius:       125,
-    children: {
-      shape:        'circle',
-      radius:       30,
-      fill:         [ 'pink', 'red' ],
-      strokeWidth:  15,
-      duration:     400,
-    }
-    });
-  
-  // add tweens to timeline:
-  timeline.add(tween3);
-  
-  timeline.play()
-  
+  //like bouton
   // when clicking the button start the timeline/animation:
   $( ".like_button" ).click(function() {
     if ($(this).hasClass('active') && $(".swiper-slide").hasClass('liked_active')|| !$(".swiper-slide").hasClass('swiper-slide_open')){
@@ -138,6 +85,6 @@ $( document ).ready(function() {
  
  
   
-  });
+  
 
 //transform: rotateY(180deg);
